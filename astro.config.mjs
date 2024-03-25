8a2c1d8b4dda03d323b84b0aa249a1262cfe02df
Anton Bubel <a.bubel@itransition.com>
2024-03-25 12:51:00 +0100

    Update main sections. Add a map of the world

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
@@ -8,5 +8,6 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  vite: { optimizeDeps: { include: ["ol"] } },
 });
